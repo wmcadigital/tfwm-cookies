b3032a3507c305da350397513fe8dff20de62bdc
Dayle Salmon <daylesalmon@gmail.com>
2021-08-09 16:34:34 +0100

    Fixed typing issues in cookie banner logic

--- src/cookie-banner/cookie-banner.ts
+++ src/cookie-banner/cookie-banner.ts
@@ -1,18 +1,20 @@
-import getCookie from 'src/helpers/getCookie';
-import setCookie from 'src/helpers/setCookie';
+import getCookie from '../helpers/getCookie';
+import setCookie from '../helpers/setCookie';
 
 const cookiesJS = () => {
-  const cookiesBanner = document.querySelector('header .wmnds-cookies-banner');
+  const cookiesBanner = document.querySelector<HTMLElement>(
+    'header .wmnds-cookies-banner',
+  );
 
   const hideCookieBanner = () => {
-    cookiesBanner.style.display = 'none';
+    if (cookiesBanner) cookiesBanner.style.display = 'none';
   };
   const showCookieBanner = () => {
-    cookiesBanner.style.display = 'block';
+    if (cookiesBanner) cookiesBanner.style.display = 'block';
   };
 
   // Check if cookie(s) created or not
-  const checkCookie = (cname) => {
+  const checkCookie = (cname: string) => {
     const isCookieCreated = getCookie(cname);
     if (isCookieCreated === '') {
       return false;
@@ -26,16 +28,17 @@ const cookiesJS = () => {
     if (document.querySelector('.wmnds-cookies-manager__preferences')) {
       hideCookieBanner();
       const cookiesOptions = document
-        .querySelector('.wmnds-cookies-manager__preferences')
-        .querySelectorAll('.wmnds-fe-checkboxes__input');
+        .querySelector<HTMLElement>('.wmnds-cookies-manager__preferences')
+        ?.querySelectorAll<HTMLFormElement>('.wmnds-fe-checkboxes__input');
       const currentOptions = [
         getCookiePolicy().essential,
         getCookiePolicy().functional,
         getCookiePolicy().performance,
       ];
-      for (let i = 0; i < cookiesOptions.length; i += 1) {
-        cookiesOptions[i].checked = currentOptions[i];
-      }
+      if (cookiesOptions)
+        for (let i = 0; i < cookiesOptions.length; i += 1) {
+          cookiesOptions[i].checked = currentOptions[i];
+        }
     }
   };
 
@@ -60,17 +63,18 @@ const cookiesJS = () => {
   };
 
   const updateAndShowSuccessMessage = () => {
-    const message = document.querySelector(
+    const message = document.querySelector<HTMLElement>(
       '.wmnds-cookies-manager__success-message',
     );
     if (message) {
-      const link = message.querySelector(
+      const link = message.querySelector<HTMLLinkElement>(
         '.wmnds-cookies-manager__previous-page a',
       );
       if (link) {
         if (
-          document.referrer === '' ||
-          document.referrer === window.location.href
+          (document.referrer === '' ||
+            document.referrer === window.location.href) &&
+          link.parentElement
         ) {
           link.parentElement.style.display = 'none';
         } else {
@@ -87,12 +91,16 @@ const cookiesJS = () => {
     if (document.querySelector('.wmnds-cookies-manager__form')) {
       const elements = document
         .querySelector('.wmnds-cookies-manager__form')
-        .querySelectorAll('.wmnds-fe-checkboxes__input');
-      const selectedOptions = [];
-      for (let i = 0; i < elements.length; i += 1) {
-        selectedOptions[i] = elements.item(i).checked;
-      }
-      setCookiePolicy(...selectedOptions);
+        ?.querySelectorAll<HTMLFormElement>('.wmnds-fe-checkboxes__input');
+      const selectedOptions: boolean[] = [];
+
+      if (elements)
+        for (let i = 0; i < elements.length; i += 1) {
+          selectedOptions[i] = elements.item(i).checked;
+        }
+      console.log(selectedOptions);
+
+      setCookiePolicy(...(selectedOptions as [boolean, boolean, boolean]));
       setCookie('cookies-preference', true, 181);
       updateAndShowSuccessMessage();
     }
@@ -116,11 +124,11 @@ const cookiesJS = () => {
   if (!isInIframe) cookiesScan();
 
   // When Accept all cookies button is triggered
-  const acceptAllCookiesBtn = document.querySelector(
+  const acceptAllCookiesBtn = document.querySelector<HTMLButtonElement>(
     '.wmnds-cookies-banner__accept-all-cookies',
   );
 
-  if (!isInIframe) {
+  if (!isInIframe && acceptAllCookiesBtn) {
     acceptAllCookiesBtn.addEventListener('click', acceptAllCookies);
     acceptAllCookiesBtn.addEventListener('keydown', (event) => {
       if (
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import cookiePolicyLogic from './cookie-policy-logic';
+import cookiesJS from './cookie-banner/cookie-banner';
 
 // Wait until everything is loaded
 window.addEventListener('load', () => {
   cookiePolicyLogic();
+  cookiesJS();
 });
