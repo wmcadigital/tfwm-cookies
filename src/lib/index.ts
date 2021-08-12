bdc273932739d5ccac5726f0653f663af42b11bf
Dayle Salmon <dayle.salmon@wmca.org.uk>
2021-08-12 13:42:40 +0100

    Updated sandbox env

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,7 @@
 import cookieBanner from './core/cookieBanner';
 import manageCookies from './core/manageCookies';
 import cookiePolicyLogic from './core/cookiePolicyLogic';
+import { getCookie } from './helpers';
 
 // Wait until everything is loaded
 const tfwmCookieLogic = () => {
@@ -9,4 +10,4 @@ const tfwmCookieLogic = () => {
   manageCookies();
 };
 
-export default tfwmCookieLogic;
+export { getCookie, tfwmCookieLogic };
